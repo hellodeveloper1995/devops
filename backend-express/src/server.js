b74eab6ad5930324027565612bc91def6201cba4
Md. Kamrul Islam <iub.kamrulislam@gmail.com>
2025-09-09 20:19:52 +0600

    add db functionality to my apps

--- backend-express/src/server.js
+++ backend-express/src/server.js
@@ -13,26 +13,22 @@ app.get('/health', (req, res) => {
     res.status(200).send('Up');
 });
 
-// console.log('Connceting to db...');
-// mongoose
-//     .connect('mongodb://mongodb/store', {
-//         auth: {
-//             username: 'kamrul',
-//             password: 'root',
-//         },
-//         connectTimeoutMS: 500,
-//     })
-//     .then(() => {
-//         console.log('Connected to DB');
-//         app.listen(3000, () => {
-//             console.log('Server listening on port 3000');
-//         });
-//     })
-//     .catch((err) => {
-//         console.log('Something went wrong');
-//         console.log(err);
-//     });
-
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-});
+console.log('Connceting to db...');
+mongoose
+    .connect('mongodb://mongodb/store', {
+        auth: {
+            username: 'kamrul',
+            password: 'root',
+        },
+        connectTimeoutMS: 500,
+    })
+    .then(() => {
+        console.log('Connected to DB');
+        app.listen(3000, () => {
+            console.log('Server listening on port 3000');
+        });
+    })
+    .catch((err) => {
+        console.log('Something went wrong');
+        console.log(err);
+    });
